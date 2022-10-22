e495d553e61e28e6793ba624586618b84b345491
Kota <maedakoutaa@gmail.com>
2022-10-22 10:22:52 +0900

    Initial Commit

---
+++ MiniApp108-Difference-Behavior&Subject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp108-Difference-Behavior&Subject
+//
+//  Created by 前田航汰 on 2022/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MiniApp108-Difference-Behavior&SubjectUITests/MiniApp108_Difference_Behavior_SubjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MiniApp108_Difference_Behavior_SubjectUITestsLaunchTests.swift
+//  MiniApp108-Difference-Behavior&SubjectUITests
+//
+//  Created by 前田航汰 on 2022/10/22.
+//
+
+import XCTest
+
+final class MiniApp108_Difference_Behavior_SubjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
